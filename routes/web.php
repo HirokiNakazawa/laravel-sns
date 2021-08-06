2b9cf3ad6dd879f51d0d2a18b809f61585f3c51f
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-08-07 08:36:37 +0900

    create-body

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-
 Auth::routes();
 
 /*
@@ -15,4 +13,5 @@ Auth::routes();
 |
 */
 
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
